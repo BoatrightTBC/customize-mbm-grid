9e730bac0effa682467a9e0d30311191b18fd2a4
Rick Boatright <boatright@tbcsoftware.com>
2017-09-22 13:15:28 -0500

    Format authors, cut at more...

--- customize-mbm-grid.php
+++ customize-mbm-grid.php
@@ -1,3 +1,5 @@
+// MBM add excerpt and authors to overlay div in book grid. 
+
 add_filter( 'mbdb_book_grid_pre_image', 'rick_boatright_add_info_to_grid', 10, 2);
 add_filter( 'mbdb_book_grid_pre_placeholder_image', 'rick_boatright_add_info_to_grid', 10, 2); 
 function rick_boatright_add_info_to_grid( $content, $book_id ) {
@@ -9,10 +11,14 @@ function rick_boatright_add_info_to_grid( $content, $book_id ) {
 	if ( $authors !== false ) {
 		$author_names =  mbdbma_get_author_names( $authors ); // author names will be comma-delimited string of author names
 	}
-	$content .= esc_html($author_names); 
+	$content .= '<p>by ' . str_replace(',', ' &',esc_html($author_names)) .'</p>'; 
 	
 	$book = MBDB()->book_factory->create_book( $book_id );
 	$summary = $book->summary;
+	$morePos = strpos($summary,'<!--more-->');
+	if ($morePos > 0) {
+	    $summary = substr($summary,0,$morePos) . "...";
+	}
 	
 	global $wp_embed;
 	$summary = $wp_embed->autoembed( $summary );
